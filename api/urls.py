cdc970108909557ff67aeacdb51ed484397f39d1
Dmitry Mukovkin <mukovkin@yandex.ru>
2017-02-12 22:38:23 +0700

    reformat urls.py

--- api/urls.py
+++ api/urls.py
@@ -30,16 +30,20 @@ urlpatterns = [
 
         url(r'^timer.get$', timer.get),
 
-        url(r'^user.list$', user.list),
-        url(r'^user.get$', user.get),
-        url(r'^user.delete$', user.delete),
+        url(r'^user.', include([
+            url(r'^list$', user.list),
+            url(r'^get$', user.get),
+            url(r'^delete$', user.delete),
+        ])),
 
-        url(r'^quest.list$', quest.list_quest),
-        url(r'^quest.take$', quest.take_quest),
-        url(r'^quest.pass$', quest.pass_answer),
-        url(r'^quest.create$', quest.create_quest),
-        url(r'^quest.save$', quest.edit_quest),
-        url(r'^quest.delete$', quest.delete_quest),
-        url(r'^quest.attempts$', quest.get_attempts),
+        url(r'^quest.', include([
+            url(r'^list$', quest.list_quest),
+            url(r'^take$', quest.take_quest),
+            url(r'^pass$', quest.pass_answer),
+            url(r'^create$', quest.create_quest),
+            url(r'^save$', quest.edit_quest),
+            url(r'^delete$', quest.delete_quest),
+            url(r'^attempts$', quest.get_attempts)
+        ]))
     ]))
 ]
