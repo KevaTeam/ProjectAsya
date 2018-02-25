f36f0985c42f2a3a23b3f88256a7b69b17d1486d
Dmitry Mukovkin <mukovkin@yandex.ru>
2018-02-26 02:14:36 +0700

    Fix collation in db

--- docker/platform/start.sh
+++ docker/platform/start.sh
@@ -14,8 +14,8 @@ echo yes | python3 /app/platform/manage.py collectstatic --noinput
 
 # Устанавливаем необходимые зависимости
 cd /app/platform/frontend/static
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-sudo apt-get install -y nodejs
+curl -sL https://deb.nodesource.com/setup_8.x | bash -
+apt-get install -y nodejs
 
 npm i
 
